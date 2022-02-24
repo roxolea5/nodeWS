52478c3860224135eb372ebc7efc5eb3e8fa5cb4
roxolea5 <roxana.olea@gmail.com>
2022-02-23 19:48:30 -0600

    Adding Router on server call

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
 
 var app = express()
 
-app.use('/', function(req, res){
-    res.send('Hola');
-})
+app.use(router)
+
+router.get('/message', function(req, res){
+    res.send('Lista de mensajes')
+});
+
+router.post('/message', function(req, res){
+    res.send('Mensaje añadido')
+});
+
+router.delete('/message', function(req, res){
+    res.send('Mensaje borrado')
+});
+
+// app.use('/', function(req, res){
+//     res.send('Hola');
+// })
 
 app.listen(3000);
 console.log("La aplicación está escuchando en http://localhost:3000")
